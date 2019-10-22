ec63b1a684eed78d87a2bf40e9e83d94fafe6dec
Wolfdimon <wolf-dimon@mail.ru>
2019-10-22 21:07:08 +0300

    Have done 4th hometask

---
+++ src/main/java/com/ifmo/lesson4/LinkedList.java
@@ -0,0 +1,88 @@
+package com.ifmo.lesson4;
+
+import java.util.List;
+
+/**
+ * Односвязный список, где каждый предыдущий
+ * элемент харнит ссылку на следующий. Список
+ * оканчивается ссылкой со значением {@code null}.
+ */
+public class LinkedList {
+    /** Ссылка на первый элемент списка. */
+    private Item head;
+
+    /**
+     * Добавляет значение в конец списка.
+     *
+     * @param val Значение, которое будет добавлено.
+     */
+    public void add(Object val) {
+        // TODO implement
+        if (head==null){
+            head=new Item(val);
+            return;
+        }
+        Item obj = head;
+        while (obj.next!=null){
+            obj=obj.next;
+        }
+        obj.next=new Item((val));
+    }
+
+    /**
+     * Извлекает значение из списка по индексу.
+     *
+     * @param i Индекс значения в списке.
+     * @return Значение, которое находится по индексу
+     * или {@code null}, если не найдено.
+     */
+    public Object get(int i) {
+        // TODO implement
+        Item obj = head;
+        int count=0;
+        while(obj!=null){
+            if (count==i){
+                return obj.value;
+            }
+            obj=obj.next;
+            count++;
+        }
+        return null;
+    }
+
+    /**
+     * Удаляет значение по индексу и возвращает
+     * удаленный элемент.
+     *
+     * @param i Индекс, по которому будет удален элемент.
+     * @return Удаленное значение или {@code null}, если не найдено.
+     */
+    public Object remove(int i) {
+        // TODO implement
+        Item obj = head;
+        if (obj==null){
+            return null;
+        }
+        int count=0;
+        while (obj.next!=null){
+            if (count==i-1){
+                Object nextValue = obj.next.value;
+                obj.next=obj.next.next;
+                return nextValue;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add("1");
+        list.add("2");
+        list.add("3");
+
+        list.remove(1);
+
+        System.out.println(list.get(1));
+    }
+}
