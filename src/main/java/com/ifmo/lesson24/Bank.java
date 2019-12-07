1af48fe81042727428f3d38ebaef6538531db73f
Wolfdimon <wolf-dimon@mail.ru>
2019-12-07 19:56:39 +0300

    lessen24 v2

--- src/main/java/com/ifmo/lesson24/Bank.java
+++ src/main/java/com/ifmo/lesson24/Bank.java
@@ -3,18 +3,13 @@ package com.ifmo.lesson24;
 import com.ifmo.lesson16.print.Commands.Ban;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class Bank {
     private Map<Long, User> users = new ConcurrentHashMap<>();
     private List<Account> accounts = new CopyOnWriteArrayList<>();
+    private LinkedBlockingQueue<Transaction> lbq = new LinkedBlockingQueue<>();
 
     private class User {
         private final long id;
@@ -67,59 +62,75 @@ public class Bank {
 
     public static void main(String[] args) {
         Bank bank = new Bank();
+
         // 1. Сгенерируйте пользователей и их аккаунты (все идентификаторы должны быть уникальны).
-        List<User> users = new ArrayList<>(10);
-        List<Account> accounts = new ArrayList<>(10);
-        Random rnd = new Random((System.currentTimeMillis()));
+        List<User> users = new ArrayList<>(20);
+        List<Account> accounts = new ArrayList<>(20);
+        //Random rnd = new Random((System.currentTimeMillis()));
         for (int i = 0; i < 10; i++) {
             users.add(bank.new User(i, "Name" + i));
             accounts.add(bank.new Account(i, i, new Random(System.currentTimeMillis()).nextInt(100000)));
         }
         // 2. Переводите деньги со случайного аккаунта на случайный в 100 потоках.
         //случайный id
-        int randomId1 = new Random().nextInt(accounts.size());
-        int randomId2 = new Random().nextInt(accounts.size());
-//        int randomId2 = (int) Math.floor(Math.random() * users.size());
+        
+
+        Thread logger = new Thread(() -> {
+            while (true) {
+                try {
+                    System.out.println(bank.lbq.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        // logger.setDaemon(true);
+        logger.start();
         ExecutorService pool = Executors.newFixedThreadPool(100);
         for (int i = 0; i < 100; i++) {
-            pool.submit(() -> {bank.transferMoney(accounts.get(randomId1), accounts.get(randomId2), new Random(System.currentTimeMillis()).nextInt(100000));
+            pool.submit(() -> {
+                bank.transferMoney(accounts.get(new Random().nextInt(accounts.size())), accounts.get(new Random().nextInt(accounts.size())), new Random(System.currentTimeMillis()).nextInt(100000));
             });
         }
         pool.shutdown();
         // Другими словами, создайте 100 потоков или пул из 100 потоков, в которых
         // выполните перевод вызовом метода transferMoney().
-
+        try {
+            logger.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
+
     // TODO Самая главная часть работы!
     public void transferMoney(Account from, Account to, long amount) {
-    /*    System.out.println("FROM: " + from.id);
-        System.out.println("TO: " + to.id);
-        System.out.println("AMOUNT: " + amount);
-        
-     */
         //попытка перевести на тот же аккаунт
         if (from.id == to.id)
             return;
         //недостаточно денег
         if (from.amount < amount) {
-            new Transaction(from.id, to.id, amount, false);
+            try {
+                lbq.put(new Transaction(from.id, to.id, amount, false));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             return;
         }
-        //совершаем перевод
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-                synchronized (from) {
-                    synchronized (to) {
-                        from.amount -= amount;
-                        to.amount += amount;
-                    }
-                }
-                new Transaction(from.id, to.id, amount, true);
+
+        synchronized (from) {
+            synchronized (to) {
+                from.amount -= amount;
+                to.amount += amount;
             }
-        };
+        }
+        try {
+            lbq.put(new Transaction(from.id, to.id, amount, true));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
 
         // 1. Атомарно и потокобезопасно перевести деньги в количестве 'amount' со счёта 'from' на счёт 'to'.
         // 2. Создать объект Transaction, содержащий информацию об операции и отправить в очередь
